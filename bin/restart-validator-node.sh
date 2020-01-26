5f6dd7a3efce4714d0e9c61a95019a2cadc00f5d
Andre Miras <andre.miras@gmail.com>
2020-01-26 18:39:35 +0100

    Drops --tty for service

--- bin/restart-validator-node.sh
+++ bin/restart-validator-node.sh
@@ -2,4 +2,4 @@
 
 
 docker pull gcr.io/prysmaticlabs/prysm/validator:latest
-docker run -it --rm -v $HOME/prysm:/data --network="host" gcr.io/prysmaticlabs/prysm/validator:latest --beacon-rpc-provider=127.0.0.1:4000 --keystore-path=/data --datadir=/data --password='password'
+docker run --interactive --rm -v $HOME/prysm:/data --network="host" gcr.io/prysmaticlabs/prysm/validator:latest --beacon-rpc-provider=127.0.0.1:4000 --keystore-path=/data --datadir=/data --password='password'
