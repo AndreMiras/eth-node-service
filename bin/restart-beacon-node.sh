2ed923ef83d8e26ac97ff91e6fed9c2359773044
Andre Miras <andre.miras@gmail.com>
2020-01-26 18:44:13 +0100

    Line wrapping

--- bin/restart-beacon-node.sh
+++ bin/restart-beacon-node.sh
@@ -2,4 +2,9 @@
 
 
 docker pull gcr.io/prysmaticlabs/prysm/beacon-chain:latest
-docker run --interactive --rm -v $HOME/prysm:/data -p 4000:4000 -p 13000:13000 --name beacon-node gcr.io/prysmaticlabs/prysm/beacon-chain:latest --datadir=/data --p2p-host-ip=$(curl -s v4.ident.me)
+docker run \
+    --interactive --rm --volume $HOME/prysm:/data \
+    --publish 4000:4000 --publish 13000:13000 \
+    --name beacon-node \
+    gcr.io/prysmaticlabs/prysm/beacon-chain:latest \
+    --datadir=/data --p2p-host-ip=$(curl -s v4.ident.me)
--- bin/restart-validator-node.sh
+++ bin/restart-validator-node.sh
@@ -2,4 +2,9 @@
 
 
 docker pull gcr.io/prysmaticlabs/prysm/validator:latest
-docker run --interactive --rm -v $HOME/prysm:/data --network="host" gcr.io/prysmaticlabs/prysm/validator:latest --beacon-rpc-provider=127.0.0.1:4000 --keystore-path=/data --datadir=/data --password='password'
+docker run \
+    --interactive --rm --volume $HOME/prysm:/data \
+    --network="host" \
+    --name validator-node \
+    gcr.io/prysmaticlabs/prysm/validator:latest \
+    --beacon-rpc-provider=127.0.0.1:4000 --keystore-path=/data --datadir=/data --password='password'
