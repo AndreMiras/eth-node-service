986ff57b9602c62eb14a688df47b262f1403ea08
Andre Miras <andre.miras@gmail.com>
2020-06-07 20:33:36 +0200

    :whale: Migrates to Topaz setup

--- bin/start-beacon-node.sh
+++ bin/start-beacon-node.sh
@@ -4,8 +4,8 @@
 test -t 1 && USE_TTY="--tty"
 docker pull gcr.io/prysmaticlabs/prysm/beacon-chain:latest
 docker run \
-    --interactive $USE_TTY --rm --volume $HOME/prysm:/data \
-    --publish 4000:4000 --publish 13000:13000 \
+    --interactive $USE_TTY --rm --volume $HOME/.eth2:/data \
+    --publish 4000:4000 --publish 13000:13000  --publish 12000:12000/udp \
     --name beacon-node \
     gcr.io/prysmaticlabs/prysm/beacon-chain:latest \
     --datadir=/data --p2p-host-ip=$(curl -s v4.ident.me)
--- bin/start-validator-node.sh
+++ bin/start-validator-node.sh
@@ -7,9 +7,9 @@ test -f $CONFIG && source $CONFIG
 
 docker pull gcr.io/prysmaticlabs/prysm/validator:latest
 docker run \
-    --interactive $USE_TTY --rm --volume $HOME/prysm:/data \
+    --interactive $USE_TTY --rm --volume $HOME/.eth2validators:/data \
     --network="host" \
     --name validator-node \
     gcr.io/prysmaticlabs/prysm/validator:latest \
-    --beacon-rpc-provider=127.0.0.1:4000 --datadir=/data \
-    --keymanager=keystore --keymanageropts='{"path": "/data/validator", "passphrase": "'"$VALIDATOR_PRIVATE_KEY_PASSWORD"'"}'
+    --keystore-path=/data \
+    --keymanager=keystore --keymanageropts='{"path": "/data", "passphrase": "'"$VALIDATOR_PRIVATE_KEY_PASSWORD"'"}'
